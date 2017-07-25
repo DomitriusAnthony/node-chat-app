03f02a98aed7d46979c4d1f90682ff018e3b96c4
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-07-25 03:01:20 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+
+	socket.emit('newMessage', {
+		from: "domitrius",
+		text: "Hey, what is going on?",
+		createdAt: 123
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	})
+
 	socket.on('disconnect' , () => {
 		console.log('User disconnected');
 	});
+
 });
 
 server.listen(port, () => {
